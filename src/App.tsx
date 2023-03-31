199fc222bf4c61a21ef9b5cdfcb7600c608eeece
Justin Sun <i@jsun969.cn>
2023-03-31 23:26:57 +0800

    feat: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from 'antd';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <Button>TEST</Button>
+    </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'antd/dist/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
