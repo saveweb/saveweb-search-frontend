5f501105f18f71cf2a28131a568c3b02035fabaf
Justin Sun <i@jsun969.cn>
2023-04-01 11:05:29 +0800

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import 'antd/dist/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
